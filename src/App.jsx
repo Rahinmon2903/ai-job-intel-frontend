97f584d5a126c82141fdb0bb1dd6e7954e39db4b
Rahinmon2903 <rahin2903@gmail.com>
2026-01-25 16:39:48 +0530

    path updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import AnalysisHistory from "./pages/AnalysisHistory";
+import SkillGapOverview from "./pages/SkillGapOverview";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/history" element={<AnalysisHistory />} />
+      <Route path="/skills" element={<SkillGapOverview />} />
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
+    </>
   );
 }
 
